892c1aa0a16bc5a1cd76cab280e6b50b6b6ca875
vivekit34 <154690314+vivekit34@users.noreply.github.com>
2023-12-23 21:26:37 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,16 @@
+//Auther@vivek
+//Problem 56- Reverse a String
+class Solution {
+    public void reverseString(char[] s) {
+        int n=s.length;
+        char t;
+        for (int i=0; i<n/2;i++){
+             if(s[i]!=s[n-i-1]){
+                 t=s[i];
+                 s[i]=s[n-i-1];
+                 s[n-i-1]=t;
+             }
+        }
+        System.out.println(s);
+    }
+}
